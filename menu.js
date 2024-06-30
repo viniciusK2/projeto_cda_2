3ab2122475c7518e4b77dffe3ad88f44f610be43
Vinicius Lima <viniciuslimadacruz6@gmail.com>
2024-06-30 12:22:35 -0300

    Inserindo o icone do whats

--- menu.js
+++ menu.js
@@ -1,15 +1,27 @@
 let menu = document.getElementById('menu-nav')
+let menuCor = document.getElementById('menu-muda-cor')
+console.log(menuCor)
+
 document.addEventListener('scroll', rolar)
 
 let ultimaPosicao = 0;
 
 function rolar(){
 
-    let posicaoAtual = window.screenY;
+    
+    let posicaoAtual = window.scrollY;
 
     if(posicaoAtual > ultimaPosicao){
-        document.getElementById('menu-nav').className = "";
+        document.getElementById('menu-nav').className = "teste";
     } else{
-        document.getElementById('menu-nav').className = "teste"
+     document.getElementById('menu-nav').className = ""
     }
+    if(posicaoAtual > 800 && posicaoAtual < 1800 || posicaoAtual > 2800 && posicaoAtual < 3800 || posicaoAtual > 4300 && posicaoAtual < 5800){
+        menuCor.style.backgroundColor = "#225522"
+    } else{
+        menuCor.style.backgroundColor = ""
+    }
+
+   
+    ultimaPosicao = posicaoAtual
 }
